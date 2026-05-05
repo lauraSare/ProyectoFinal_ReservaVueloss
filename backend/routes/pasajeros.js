37856f87b3ca8cc463ebc161df782a84057032da
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-05-05 15:42:52 -0600

    agregar rutas en pasajeros

---
+++ backend/routes/pasajeros.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  obtenerPasajeros, 
+  obtenerPasajeroPorId, 
+  actualizarPasajero, 
+  eliminarPasajero 
+} = require('../controllers/pasajeroController');
+
+// Obtener todos los pasajeros
+router.get('/', obtenerPasajeros);
+
+// Obtener un pasajero por ID
+router.get('/:id', obtenerPasajeroPorId);
+
+// Actualizar un pasajero
+router.put('/:id', actualizarPasajero);
+
+// Eliminar un pasajero
+router.delete('/:id', eliminarPasajero);
+
+module.exports = router;
