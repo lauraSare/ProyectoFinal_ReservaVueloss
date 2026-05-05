d404429cc0c42d78382ef65ddfa95cd7fe01ae54
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-05-05 15:36:06 -0600

    agregar obtenerReservaPorId

--- backend/controllers/reservaController.js
+++ backend/controllers/reservaController.js
@@ -15,4 +15,23 @@ const obtenerReservas = async (req, res) => {
   }
 };
 
-module.exports = { obtenerReservas };
+// Obtener una reserva por ID
+const obtenerReservaPorId = async (req, res) => {
+  try {
+    const reserva = await Reserva.findByPk(req.params.id, {
+      include: [
+        { model: Pasajero, attributes: { exclude: ['password'] } },
+        { model: Vuelo },
+        { model: ReservaAsiento, include: [{ model: Asiento }] }
+      ]
+    });
+    if (!reserva) {
+      return res.status(404).json({ mensaje: 'Reserva no encontrada' });
+    }
+    res.json(reserva);
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error al obtener reserva', error: error.message });
+  }
+};
+
+module.exports = { obtenerReservas, obtenerReservaPorId };
